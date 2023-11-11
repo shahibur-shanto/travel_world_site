ac33cedf9f541db9e1b56a9c36b8e841c65f53ac
shahibur-shanto <shahibur407@gmail.com>
2023-11-11 13:41:41 +0600

    test again

--- src/components/Forms/ACDepartmentField.tsx
+++ src/components/Forms/ACDepartmentField.tsx
@@ -2,31 +2,34 @@
 import FormSelectField, { SelectOptions } from "./FormSelectField";
 
 type ACDepartmentFieldProps = {
-  name: string;
-  label?: string;
+	name: string;
+	label?: string;
 };
 
 const ACDepartmentField = ({ name, label }: ACDepartmentFieldProps) => {
-  // const { data, isLoading } = useAcademicDepartmentsQuery({
-  //   limit: 100,
-  //   page: 1,
-  // });
-  // const academicDepartments = data?.academicDepartments;
-  // const acDepartmentOptions = academicDepartments?.map((acDepartment: any) => {
-  //   console.log(acDepartment?.id);
-  //   return {
-  //     label: acDepartment?.title,
-  //     value: acDepartment?.id,
-  //   };
-  // });
+	// const { data, isLoading } = useAcademicDepartmentsQuery({
+	//   limit: 100,
+	//   page: 1,
+	// });
+	// const academicDepartments = data?.academicDepartments;
+	// const acDepartmentOptions = academicDepartments?.map((acDepartment: any) => {
+	//   console.log(acDepartment?.id);
+	//   return {
+	//     label: acDepartment?.title,
+	//     value: acDepartment?.id,
+	//   };
+	// });
 
-  return (
-    <FormSelectField
-      name={name}
-      label={label}
-      options={acDepartmentOptions as SelectOptions[]}
-    />
-  );
+	return (
+		<>
+			<h1>Hi</h1>
+		</>
+		// <FormSelectField
+		//   name={name}
+		//   label={label}
+		//   options={acDepartmentOptions as SelectOptions[]}
+		// />
+	);
 };
 
 export default ACDepartmentField;
