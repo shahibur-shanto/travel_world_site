fce60dfa73fed9d887568cd4eda37d53a2663834
shahibur-shanto <shahibur407@gmail.com>
2023-11-11 12:23:13 +0600

    test again

--- src/components/Forms/ACDepartmentField.tsx
+++ src/components/Forms/ACDepartmentField.tsx
@@ -1,4 +1,4 @@
-import { useAcademicDepartmentsQuery } from "@/redux/api/academic/departmentApi";
+// import { useAcademicDepartmentsQuery } from "@/redux/api/academic/departmentApi";
 import FormSelectField, { SelectOptions } from "./FormSelectField";
 
 type ACDepartmentFieldProps = {
@@ -7,18 +7,18 @@ type ACDepartmentFieldProps = {
 };
 
 const ACDepartmentField = ({ name, label }: ACDepartmentFieldProps) => {
-  const { data, isLoading } = useAcademicDepartmentsQuery({
-    limit: 100,
-    page: 1,
-  });
-  const academicDepartments = data?.academicDepartments;
-  const acDepartmentOptions = academicDepartments?.map((acDepartment: any) => {
-    console.log(acDepartment?.id);
-    return {
-      label: acDepartment?.title,
-      value: acDepartment?.id,
-    };
-  });
+  // const { data, isLoading } = useAcademicDepartmentsQuery({
+  //   limit: 100,
+  //   page: 1,
+  // });
+  // const academicDepartments = data?.academicDepartments;
+  // const acDepartmentOptions = academicDepartments?.map((acDepartment: any) => {
+  //   console.log(acDepartment?.id);
+  //   return {
+  //     label: acDepartment?.title,
+  //     value: acDepartment?.id,
+  //   };
+  // });
 
   return (
     <FormSelectField
--- src/components/Forms/ACFacultyField.tsx
+++ src/components/Forms/ACFacultyField.tsx
@@ -1,31 +1,34 @@
-import { useAcademicFacultiesQuery } from "@/redux/api/academic/facultyApi";
+// import { useAcademicFacultiesQuery } from "@/redux/api/academic/facultyApi";
 import FormSelectField, { SelectOptions } from "./FormSelectField";
 
 type ACFacultyFieldProps = {
-  name: string;
-  label: string;
+	name: string;
+	label: string;
 };
 
 const ACFacultyField = ({ name, label }: ACFacultyFieldProps) => {
-  const { data, isLoading } = useAcademicFacultiesQuery({
-    limit: 100,
-    page: 1,
-  });
-  const academicFaculties = data?.academicFaculties;
-  const acFacultyOptions = academicFaculties?.map((acFaculty) => {
-    return {
-      label: acFaculty?.title,
-      value: acFaculty?.id,
-    };
-  });
+	// const { data, isLoading } = useAcademicFacultiesQuery({
+	//   limit: 100,
+	//   page: 1,
+	// });
+	// const academicFaculties = data?.academicFaculties;
+	// const acFacultyOptions = academicFaculties?.map((acFaculty) => {
+	//   return {
+	//     label: acFaculty?.title,
+	//     value: acFaculty?.id,
+	//   };
+	// });
 
-  return (
-    <FormSelectField
-      name={name}
-      label={label}
-      options={acFacultyOptions as SelectOptions[]}
-    />
-  );
+	return (
+		<>
+			<h1>Hi</h1>
+		</>
+		// <FormSelectField
+		//   name={name}
+		//   label={label}
+		//   options={acFacultyOptions as SelectOptions[]}
+		// />
+	);
 };
 
 export default ACFacultyField;
