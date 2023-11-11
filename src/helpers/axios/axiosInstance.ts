a6aae43600e4a7110b896b150a6541bcf3491e11
shahibur-shanto <shahibur407@gmail.com>
2023-11-11 19:22:35 +0600

    cors

--- src/helpers/axios/axiosInstance.ts
+++ src/helpers/axios/axiosInstance.ts
@@ -8,6 +8,7 @@ const instance = axios.create();
 instance.defaults.headers.post["Content-Type"] = "application/json";
 instance.defaults.headers["Accept"] = "application/json";
 instance.defaults.timeout = 60000;
+instance.defaults.withCredentials = true;
 
 // Add a request interceptor
 instance.interceptors.request.use(
